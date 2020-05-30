d1cd71332a375557b2a19b594b4ceda374d1f9fb
Alex Gladd <alex@alexgladd.com>
2020-05-30 21:00:16 +0200

    Temporarily add vendor css to head

--- src/components/SiteMetadata.jsx
+++ src/components/SiteMetadata.jsx
@@ -30,6 +30,10 @@ export default function SiteMetadata({ title, description, lang }) {
       <meta property="twitter:creator" content={siteMetadata.author} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={metaDescription} />
+
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css" integrity="sha256-CuUPKpitgFmSNQuPDL5cEfPOOJT/+bwUlhfumDJ9CI4=" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/brands.min.css" integrity="sha256-wfbbsQFYKnizQi/WLPXS3wVDu0Dpi2yUQpZBDsb2H1s=" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/solid.min.css" integrity="sha256-pIAzc/BIIo/hSvtNEDIiMTBtR9EfK3COmnH2pt8cPDY=" crossorigin="anonymous" />
     </Helmet>
   );
 }
