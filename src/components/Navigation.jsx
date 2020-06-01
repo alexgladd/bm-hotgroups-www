08a2ff0dc1b73bb856c13c9d9f4dbe7b3e7372fe
Alex Gladd <alex@alexgladd.com>
2020-06-01 14:36:21 +0200

    Remove unused import

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { Link } from 'gatsby';
 import { colors, sizes, transitions } from './theme';
