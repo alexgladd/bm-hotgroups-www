5c62e2d1ab2cb17db541ae7158a7c927ac043771
Alex Gladd <alex@alexgladd.com>
2021-05-03 11:12:44 +0200

    Update 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import Section from '../components/Section';
 
 const NotFoundPage = () => (
   <Layout title="Whoops!">
-    <h1>NOT FOUND</h1>
-    <p>Looks like you're lost! Try heading <Link to="/">home</Link>.</p>
+    <Section span title="404 Not Found">
+      <p>Looks like you're lost! Try heading <Link to="/">home</Link>.</p>
+    </Section>
   </Layout>
 );
 
